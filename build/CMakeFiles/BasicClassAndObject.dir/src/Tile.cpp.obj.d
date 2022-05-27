f5bc1edeb1cfe0baf6b15641773286270f21a340
FinlieJoel <102375783+FinlieJoel@users.noreply.github.com>
2022-05-27 17:11:30 +0100

    Delete build directory

--- build/CMakeFiles/BasicClassAndObject.dir/src/Tile.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/BasicClassAndObject.dir/src/Tile.cpp.obj: \
- C:/Projects/GameOfLife/src/Tile.cpp \
- C:/Projects/GameOfLife/include/Tile.h
--- build/CMakeFiles/BasicClassAndObject.dir/src/TileRules.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/BasicClassAndObject.dir/src/TileRules.cpp.obj: \
- C:/Projects/GameOfLife/src/TileRules.cpp \
- C:/Projects/GameOfLife/include/TileRules.h
